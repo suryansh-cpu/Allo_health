5bd29ee8673c8690d7f4553fda6854832b91f3ee
SURYANSH BADERA <lemononator23@gmail.com>
2026-06-04 17:59:46 +0530

    git ignore updated

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ae70a944dac483ed"; }
+/******/ 		__webpack_require__.h = function() { return "68611dd1ad80ba58"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
